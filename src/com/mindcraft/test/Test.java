8b3c31994eec9d7af5052087736961c8c88ef6c2
bornt2code <born.to.code@outlook.com>
2018-11-18 18:33:08 +0530

    New employee class and code change in main

--- src/com/mindcraft/test/Test.java
+++ src/com/mindcraft/test/Test.java
@@ -1,10 +1,18 @@
 package com.mindcraft.test;
 
+import com.mindcraft.pojo.Employee;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World !!");
+
+		System.out.println("Creating employee object");
+
+		Employee employee = new Employee(101, "Vivek", 2000);
+
+		System.out.println(employee);
 	}
 
 }
